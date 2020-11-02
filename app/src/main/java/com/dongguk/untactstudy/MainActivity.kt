529fa1ac5b6b074132c30e25ad4e2454707865e8
wonny7447 <wonny7447@naver.com>
2020-11-03 00:19:34 +0900

    2020.11.02 / 백지원 / 구글 로그인 연동 - 구글 로그인 기능 추가

--- app/src/main/java/com/dongguk/untactstudy/MainActivity.kt
+++ app/src/main/java/com/dongguk/untactstudy/MainActivity.kt
@@ -1,13 +1,86 @@
 package com.dongguk.untactstudy
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
+
+    // firebase 인증을 위한 변수
+    var auth : FirebaseAuth ? = null
+
+    // 구글 로그인 연동에 필요한 변수
+    var googleSignInClient : GoogleSignInClient ? = null
+    var GOOGLE_LOGIN_CODE = 9001
+
+
+    // onCreate는 Acitivity가 처음 실행 되는 상태에 제일 먼저 호출되는 메소드로 여기에 실행시 필요한 각종 초기화 작업을 적어줌
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 로그인 페이지가 첫화면임 (activity_main.xml의 레이아웃 사용)
         setContentView(R.layout.activity_main)
 
-        // 커밋 테스트
-    }
+        // firebaseauth를 사용하기 위한 인스턴스 get
+        auth = FirebaseAuth.getInstance()
+
+        // xml에서 구글 로그인 버튼 코드 가져오기
+        var google_sign_in_button = findViewById<SignInButton>(R.id.google_sign_in_button)
+
+        // 구글 로그인 버튼 클릭 시 이벤트 : googleLogin function 실행
+        google_sign_in_button.setOnClickListener {
+            googleLogin()
+        }
+
+        // 구글 로그인을 위해 구성되어야 하는 코드 (Id, Email request)
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+    } // onCreate
+
+    fun googleLogin() {
+        var signInIntent = googleSignInClient?.signInIntent
+        startActivityForResult(signInIntent, GOOGLE_LOGIN_CODE)
+    } // googleLogin
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == GOOGLE_LOGIN_CODE) {
+            var result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result != null) {
+                if(result.isSuccess) {
+                    var account = result.signInAccount
+                    firebaseAuthWithGoogle(account)
+                }
+            }
+        } //if
+    } // onActivityResult
+
+    fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
+        var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
+        auth?.signInWithCredential(credential)
+            ?.addOnCompleteListener {
+                task ->
+                if(task.isSuccessful) {
+                    // 로그인 성공 시
+                    Toast.makeText(this,  "success", Toast.LENGTH_LONG).show()
+                    startActivity(Intent (this, StudyRecommendActivity::class.java))
+                } else {
+                    // 로그인 실패 시
+                    Toast.makeText(this,  task.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+    } //firebaseAuthWithGoogle
 }
