e0b1a89af81b137018288f08f6e0cec92fb53c1d
wonny7447 <wonny7447@naver.com>
2020-11-03 13:10:31 +0900

    2020.11.03 / 백지원 / RealTimeDatabase 연동

--- app/src/main/java/com/dongguk/untactstudy/StudyRecommendActivity.kt
+++ app/src/main/java/com/dongguk/untactstudy/StudyRecommendActivity.kt
@@ -1,29 +1,46 @@
 package com.dongguk.untactstudy
 
-import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class StudyRecommendActivity : AppCompatActivity() {
+
+    // 로그아웃 구현을 위한 변수
+    var auth : FirebaseAuth ?= null
+    var googleSignInClient : GoogleSignInClient ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_study_recommend)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
+        // 구글 로그아웃을 위해 로그인 세션 가져오기
+        var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // firebaseauth를 사용하기 위한 인스턴스 get
+        auth = FirebaseAuth.getInstance()
+
+        // 구글 로그아웃 버튼 ID 가져오기
+        var google_sign_out_button = findViewById<Button>(R.id.google_sign_out_button)
+
+        // 구글 로그아웃 버튼 클릭 시 이벤트
+        google_sign_out_button.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            googleSignInClient?.signOut()
+
+            var logoutIntent = Intent (this, MainActivity::class.java)
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(logoutIntent)
+        }
+
+    } //onCreate
 }
