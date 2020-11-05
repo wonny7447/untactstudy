812e182a3d932a31a79422e82740a8acb6f5962b
yoonj98 <69336270+yoonj98@users.noreply.github.com>
2020-11-06 01:07:31 +0900

    2020.11.06 / 이윤정 / 최초 로그인 및 분류창 (미완)

--- app/src/main/java/com/dongguk/untactstudy/MainActivity.kt
+++ app/src/main/java/com/dongguk/untactstudy/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.dongguk.untactstudy
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
@@ -15,7 +15,7 @@ import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
-    // firebase 인증을 위한 변수 (박민기 커밋 테스트.....)
+    // firebase 인증을 위한 변수
     var auth : FirebaseAuth ? = null
 
     // 구글 로그인 연동에 필요한 변수
@@ -47,7 +47,6 @@ class MainActivity : AppCompatActivity() {
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
-
     } // onCreate
 
     fun googleLogin() {
@@ -71,17 +70,29 @@ class MainActivity : AppCompatActivity() {
     fun firebaseAuthWithGoogle(account: GoogleSignInAccount?) {
         var credential = GoogleAuthProvider.getCredential(account?.idToken, null)
         auth?.signInWithCredential(credential)
-            ?.addOnCompleteListener {
-                task ->
+            ?.addOnCompleteListener { task ->
                 if(task.isSuccessful) {
                     // 로그인 성공 시
-                    Toast.makeText(this,  "success", Toast.LENGTH_LONG).show()
-                    startActivity(Intent (this, StudyRecommendActivity::class.java))
+                    val isNew = task.result!!.additionalUserInfo!!.isNewUser
+                    //Toast.makeText(this, "success", Toast.LENGTH_LONG).show()
+
+                    if(isNew){  //최초 로그인 유저인 경우
+                        Toast.makeText(this, "New User", Toast.LENGTH_LONG).show()  //test
+                        startActivity(Intent(this, StudyCategoryActivity::class.java))
+                        finish()
+                    }
+                    else{   //기존 로그인 유저인 경우
+                        Toast.makeText(this, "Old User", Toast.LENGTH_LONG).show()
+                    }
+
+                    //startActivity(Intent(this, StudyRecommendActivity::class.java))
+
                 } else {
                     // 로그인 실패 시
-                    Toast.makeText(this,  task.exception?.message, Toast.LENGTH_LONG).show()
+                    Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
                 }
             }
     } //firebaseAuthWithGoogle
+
+
 }
-//윤성우 테스트 커밋 (지워주세요)
