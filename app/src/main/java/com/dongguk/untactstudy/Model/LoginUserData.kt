24feef725e4bb98f9633049c22936677d2cea904
wonny7447 <wonny7447@naver.com>
2020-11-08 23:12:47 +0900

    2020.11.08 / 백지원 / 구글 로그인 정보 FireStore DB에 저장  - DB : loginUserData

---
+++ app/src/main/java/com/dongguk/untactstudy/Model/LoginUserData.kt
@@ -0,0 +1,7 @@
+package com.dongguk.untactstudy.Model
+
+class LoginUserData(val uid : String,
+                    val userName : String,
+                    val userEmail : String,
+                    val userPhotoUrl : String,
+                    val time : Long)
