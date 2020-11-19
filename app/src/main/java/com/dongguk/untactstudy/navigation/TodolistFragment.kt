c608e7916ed5dc3acdd450429a19e9c97dd1921f
wonny7447 <wonny7447@naver.com>
2020-11-19 17:43:16 +0900

    2020.11.19 / 백지원 / 이전 주차 버튼 클릭 시 전주의 to-do 리스트 볼 수 있도록 작업 완료

--- app/src/main/java/com/dongguk/untactstudy/navigation/TodolistFragment.kt
+++ app/src/main/java/com/dongguk/untactstudy/navigation/TodolistFragment.kt
@@ -1,12 +1,15 @@
 package com.dongguk.untactstudy.navigation
 
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.CompoundButton
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -36,28 +39,91 @@ class TodolistFragment : Fragment(){
 
     var studyStartDate : Long = 0
     var thisWeek : Long = 0
+    var currentWeek : Long = 0
     var studyRoomNumber : String = "0"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         var view = LayoutInflater.from(activity).inflate(R.layout.fragment_todolist, container, false)
 
         var adapter = GroupAdapter<GroupieViewHolder>()
-
-        view.todoRecyclerView.adapter = TodoRecyclerViewAdapter()
+        view.todoRecyclerView.adapter = TodoRecyclerViewAdapter(currentWeek)
         view.todoRecyclerView.layoutManager = LinearLayoutManager(activity)
+
+        // 이전 주차 데이터 확인을 위한 버튼
+        var prevWeekButton = view?.findViewById<ImageButton>(R.id.prevWeekButton)
+
+        // 이전 주차 to-do 확인을 위한 이벤트 리스너
+        prevWeekButton?.setOnClickListener {
+            if(currentWeek > 1) {
+                currentWeek = currentWeek - 1
+
+                view.todoRecyclerView.removeAllViewsInLayout()
+                view.todoRecyclerView.adapter = TodoRecyclerViewAdapter(currentWeek)
+            }
+        }
+
+        // 차주 데이터 확인을 위한 버튼
+        var nextWeekButton = view?.findViewById<ImageButton>(R.id.nextWeekButton)
+
+        // 차주 to-do 확인을 위한 이벤트 리스너
+        nextWeekButton?.setOnClickListener {
+            Log.e(TAG, "차주 데이터 노출")
+        }
 
         return view
     } // onCreateView
 
-    inner class TodoRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    inner class TodoRecyclerViewAdapter(temp : Long) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
         //var todoList : MutableList<String> = listOf<String>("") as MutableList<String>
         var todoList = ArrayList<String>()
 
-        init {
 
+        init {
+            data(temp)
+
+        } // init
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_list_row, parent, false)
+            return TodoCustomViewHolder(view)
+        } //onCreateViewHolder
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val todoText = holder.itemView.todoText
+
+            thisWeekText.text = currentWeek.toString()+"주차 TO-DO"
+
+            // todo_list_row의 todoText에 값 보여주기
+            todoText.text = todoList[position].toString()
+            Log.e(TAG, "position : "+ position+", todoText.text : "+todoText.text)
+
+            // todo_list_row에 대한 클릭 이벤트
+            holder.itemView.setOnClickListener{
+                Log.e(TAG, "click : "+position)
+            }
+
+            // todo_list_row의 체크박스에 대한 클릭 이벤트
+            holder.itemView.checkBox.setOnCheckedChangeListener{ compoundButton: CompoundButton, isChecked: Boolean ->
+                if(isChecked) {
+                    // 사용자 DB > to-do 리스트에서 해당 항목 true로 변경
+                    Log.e(TAG, "click : "+position+", checked")
+                } else {
+                    // 사용자 DB > to-do 리스트에서 해당 항목 false로 변경
+                    Log.e(TAG, "click : "+position+", UN checked")
+                }
+            }
+
+        } //onBindViewHolder
+
+        override fun getItemCount(): Int {
+            return todoList.size
+        } //getItemCount
+
+        fun data(temp : Long){
             var userData : LoginUserData ? = null
             var studyData : StudyModel ?= null
+            var week = temp
 
             FirebaseFirestore.getInstance().collection("loginUserData")
                 .document(FirebaseAuth.getInstance().uid.toString())
@@ -99,21 +165,23 @@ class TodolistFragment : Fragment(){
                                             var diff : Long = studyStartDate - today
                                             diff /= (24 * 60 * 60 * 1000)
                                             diff = Math.abs(diff)
-
-                                            if((diff % 7) > 0) {
-                                                thisWeek = (diff / 7) + 1
-                                            } else {
-                                                thisWeek = (diff / 7)
+                                            if(thisWeek < 1) {
+                                                if ((diff % 7) > 0) {
+                                                    thisWeek = (diff / 7) + 1
+                                                } else {
+                                                    thisWeek = (diff / 7)
+                                                }
+                                                week = thisWeek
                                             }
-                                            Log.e(TAG, "diff : "+diff+", thisWeek : "+thisWeek)
+                                            Log.e(TAG, "diff : "+diff+", thisWeek : "+thisWeek+", currentWeek : "+currentWeek+", week : "+week)
 
                                             FirebaseFirestore.getInstance()
                                                 .collection("studyInfo")
                                                 .document(studyRoomNumber)
                                                 .collection("todoList")
-                                                .document(thisWeek.toString())
+                                                .document(week.toString())
                                                 .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                                                    Log.e(TAG, "db thisWeek : "+thisWeek+", ")
+                                                    Log.e(TAG, "db week : "+week+", ")
                                                     todoList.clear()
                                                     if(querySnapshot == null) return@addSnapshotListener
                                                     Log.e(TAG, "querySnapShot : "+querySnapshot.toObject(TodoData::class.java)!!.list.toString())
@@ -122,7 +190,7 @@ class TodolistFragment : Fragment(){
                                                         todoList.add(tempList[i-1].toString())
                                                         notifyDataSetChanged()
                                                     }
-
+                                                    currentWeek = week
                                                 } //addSnapshotListener
                                         } //if-else
                                     } //if(task.isSuccessful)
@@ -130,45 +198,7 @@ class TodolistFragment : Fragment(){
                         }  //if-else
                     } //if(task.isSuccessful)
                 } //addOnCompleteListener
-        } // init
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.todo_list_row, parent, false)
-            return TodoCustomViewHolder(view)
-        } //onCreateViewHolder
-
-        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-            val todoText = holder.itemView.todoText
-            val currentViewWeek = thisWeek
-
-            thisWeekText.text = currentViewWeek.toString()+"주차 TO-DO"
-
-            // todo_list_row의 todoText에 값 보여주기
-            todoText.text = todoList[position].toString()
-            Log.e(TAG, "position : "+ position+", todoText.text : "+todoText.text)
-
-            // todo_list_row에 대한 클릭 이벤트
-            holder.itemView.setOnClickListener{
-                Log.e(TAG, "click : "+position)
-
-            }
-
-            // todo_list_row의 체크박스에 대한 클릭 이벤트
-            holder.itemView.checkBox.setOnCheckedChangeListener{ compoundButton: CompoundButton, isChecked: Boolean ->
-                if(isChecked) {
-                    // 사용자 DB > to-do 리스트에서 해당 항목 true로 변경
-                    Log.e(TAG, "click : "+position+", checked")
-                } else {
-                    // 사용자 DB > to-do 리스트에서 해당 항목 false로 변경
-                    Log.e(TAG, "click : "+position+", UN checked")
-                }
-            }
-
-        } //onBindViewHolder
-
-        override fun getItemCount(): Int {
-            return todoList.size
-        } //getItemCount
+        }
 
         inner class TodoCustomViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
     } // class : TodoRecyclerViewAdapter
